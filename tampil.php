6b99f55ec843c617cf12cbee0763fac3c0b6a740
Thoms251102 <thomaswidianto2@gmail.com>
2022-09-29 08:06:34 -0600

    first commit

---
+++ tampil.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan Pertama</title>
+</head>
+<body>
+<p>Latihan pertama  saya belajar framework codeigniter</p>
+<p>Framework codeigniter sangat mudah di pelajari</p>
+</body>
+</html>
